5c56fbc9b645e152e46b310fc5c6d94e86867ae0
Minh Huy <liemminhhuy2003@gmail.com>
2024-05-14 20:02:50 +0700

    create forumanswer

--- sm-mobileapp/android/app/src/main/java/com/anonymous/smmobileapp/MainActivity.kt
+++ sm-mobileapp/android/app/src/main/java/com/anonymous/smmobileapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.smmobileapp
+package com.anonymous.smmobileapp
 
 import android.os.Build
 import android.os.Bundle
--- sm-mobileapp/android/app/src/main/java/com/anonymous/smmobileapp/MainApplication.kt
+++ sm-mobileapp/android/app/src/main/java/com/anonymous/smmobileapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.smmobileapp
+package com.anonymous.smmobileapp
 
 import android.app.Application
 import android.content.res.Configuration
